400b4479128b7e96d6ed151d5d3ea694c8ef3d2c
Ole Voldsæter <ole.voldsater@nsd.uib.no>
2018-05-14 09:58:33 +0200

    debug destroy buttons

--- src/cljs/asteroids/core.cljs
+++ src/cljs/asteroids/core.cljs
@@ -4,8 +4,8 @@
               [accountant.core :as accountant]
               [reagent.format :refer [format]]))
 
-(defonce app-state (atom {:asteroids '({:x 20 :y 20 :size 2 :speed-x 2 :speed-y 1 :rotation 13 :destroyed true}
-                                      {:x 795 :y 8 :size 2 :speed-x -2 :speed-y 3 :rotation 184 :destroyed true})}))
+(defonce app-state (atom {:asteroids '({:x 20 :y 20 :size 4 :speed-x 2.2 :speed-y 1.1 :rotation 13 :key 1}
+                                      {:x 795 :y 8 :size 4 :speed-x -1.9 :speed-y 1.3 :rotation 184 :key 2})}))
 
 (defn wraparound [val increment max-val min-val]
   (let [new-val (+ val increment)]
@@ -25,10 +25,14 @@
   )
 
 (defn split-asteroid [roid]
-  (list {:x (:x roid) , :y (:y roid) , :speed-x (:speed-x roid) ,
-         :speed-y (* 1.5 (:speed-y roid)) , :size (/ (:size roid) 2) , :rotation (rand-int 360)}
+  (if (> (:size roid) 1)
+    (list {:x (:x roid) , :y (:y roid) , :speed-x (:speed-x roid) ,
+         :speed-y (* 1.5 (:speed-y roid)) , :size (/ (:size roid) 2) , :rotation (rand-int 360) , :key (rand-int 1000000)}
         {:x (:x roid) , :y (:y roid) , :speed-y (:speed-y roid) ,
-         :speed-x (* 1.5 (:speed-x roid)) , :size (/ (:size roid) 2) , :rotation (rand-int 360)})
+         :speed-x (* 1.5 (:speed-x roid)) , :size (/ (:size roid) 2) , :rotation (rand-int 360) , :key (rand-int 1000000)})
+    '()
+    )
+
   )
 
 (defn split-destroyed-asteroids [roids]
@@ -50,7 +54,7 @@
 ;; Views
 
 (defn asteroid [props]
-  [:polygon {:points "-20,-10 -5,-5 -10,-20 8,-20 20,-10 20,10 13,21 -11,24 -8,8 -20,10"
+  [:polygon {:points "-18,-8 -3,-5 -8,-18 6,-18 18,-8 18,8 11,19 -9,22 -6,6 -18,8"
              :fill "none"
              :transform (format "translate (%d %d) scale(%d) rotate(%d)"
                             (:x props) (:y props)
@@ -60,23 +64,24 @@
 
 (defn asteroid-debug-info [props]
   [:div.entityDebug
-    [:div [:strong "Location: "] (:x props) ", " (:y props)]
-  ;  [:button "Destroy"]
+    [:div [:strong "Id: "] (:key props) ]
+    [:div [:strong "Location: "] (format "%.2f , %.2f" (:x props) (:y props))]
+    [:button {:on-click (fn [] (swap! app-state assoc :asteroids (map #(if (= (:key %) (:key props)) (assoc % :destroyed true) %) (:asteroids @app-state))))} "Destroy" ]
   ]
   )
 
 (defn screen [asteroids]
-  (js/setTimeout update-all-asteroids! 40)
+  (js/setTimeout update-all-asteroids! 80)
   [:svg {:viewBox "0 0 800 600"}
     (for [astr asteroids]
-      ^{:key astr} [asteroid astr]
+      ^{:key (:key astr)} [asteroid astr]
     )]
   )
 
 (defn entity-list [entities]
   [:div#entitylist
   (for [entity entities]
-    ^{:key entity} [asteroid-debug-info entity]
+    ^{:key (:key entity)} [asteroid-debug-info entity]
     )]
   )
 
